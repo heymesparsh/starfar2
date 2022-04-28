f8f992c2172439fddd2fca722aac4b21ddd16e8d
Sparsh Aggarwal <heymesparsh@gmail.com>
2022-04-28 16:12:56 +0530

    Updates

---
+++ precache-manifest.ef26a611f2afef044658e391ae9b0625.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d379706e4c8fb1dc2d0de4ef6fac1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92588694cb05ceb501f7",
+    "url": "/static/css/2.1899e22e.chunk.css"
+  },
+  {
+    "revision": "64996f8e0be9e7c124da",
+    "url": "/static/css/main.9644fa91.chunk.css"
+  },
+  {
+    "revision": "92588694cb05ceb501f7",
+    "url": "/static/js/2.31f537e1.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.31f537e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64996f8e0be9e7c124da",
+    "url": "/static/js/main.9cce5701.chunk.js"
+  },
+  {
+    "revision": "5f534a0d9dc73e61b349",
+    "url": "/static/js/runtime-main.4252001f.js"
+  },
+  {
+    "revision": "d40e0f245a51165ecd54e30df849e4ff",
+    "url": "/static/media/03.d40e0f24.jpg"
+  },
+  {
+    "revision": "ed6e77c58a992432fc2f8f03d2ad2be0",
+    "url": "/static/media/h1.ed6e77c5.jpg"
+  },
+  {
+    "revision": "c8328ffb736f04bdfbaed56ac9a90e05",
+    "url": "/static/media/h2.c8328ffb.jpg"
+  },
+  {
+    "revision": "f2aafb2fe7bfd00ece7ca2e9aee4ccb2",
+    "url": "/static/media/h3.f2aafb2f.jpg"
+  },
+  {
+    "revision": "352a1184343ced96a916ee2cb6492c03",
+    "url": "/static/media/logo.352a1184.jpg"
+  },
+  {
+    "revision": "1f6ac2d96c8302fd5f7dff6d6ba90604",
+    "url": "/static/media/spks.1f6ac2d9.png"
+  },
+  {
+    "revision": "764d0d70bb8bdcaa3f3f1f91f8b49f20",
+    "url": "/static/media/spks2.764d0d70.jpg"
+  },
+  {
+    "revision": "22c6d9d19ff8e09f8a96c04e77d53b8e",
+    "url": "/static/media/spks3.22c6d9d1.png"
+  },
+  {
+    "revision": "5bb51a2011ae922210b6397a7224b53e",
+    "url": "/static/media/star6.5bb51a20.png"
+  },
+  {
+    "revision": "66d4397d6c86a16b7f2091037781fad9",
+    "url": "/static/media/star7.66d4397d.png"
+  },
+  {
+    "revision": "d4ef042acbe021adc963de36fc9a87a2",
+    "url": "/static/media/star_1.d4ef042a.jpg"
+  },
+  {
+    "revision": "b8e6088e014098c39778716e10bf6953",
+    "url": "/static/media/star_2.b8e6088e.jpg"
+  },
+  {
+    "revision": "6f2d982864938073f03a6ef318c36de3",
+    "url": "/static/media/star_3.6f2d9828.jpg"
+  },
+  {
+    "revision": "f9a28551872ca294fa67ba7a7cbb8c9c",
+    "url": "/static/media/star_4.f9a28551.jpg"
+  }
+]);
